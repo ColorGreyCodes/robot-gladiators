e45a10406d18ad0c74de98084124de80f75688e6
AyYo-Winston <105572024+AyYo-Winston@users.noreply.github.com>
2022-06-27 12:03:43 -0400

    first console log

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+function fight() {
+    // var playerName = window.prompt("What is your robot's name?");
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is you robot's name?")
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName + "!");
+
+var name = "Anthony";
+console.log(name);
+
+// fight();
+
